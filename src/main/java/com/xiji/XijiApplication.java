99c6bb7094844b4d919045cedee56e8bf010e42e
duyuanyuan <18653425352@163.com>
2026-02-12 14:04:19 +0800

    feat(app): 启用异步任务支持
    - 添加 EnableAsync 注解启用异步方法执行

--- src/main/java/com/xiji/XijiApplication.java
+++ src/main/java/com/xiji/XijiApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * 玺记系统启动类
@@ -12,6 +13,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @MapperScan("com.xiji.mapper")
 @ServletComponentScan("com.xiji.filter")
+@EnableAsync
 public class XijiApplication {
 
     public static void main(String[] args) {
